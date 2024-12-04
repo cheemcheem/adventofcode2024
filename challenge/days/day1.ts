2f71a83dad32d9e9fb82f7d733c8edd47d4fb2e2
Kathan Cheema <19304291+cheemcheem@users.noreply.github.com>
2024-12-04 01:13:22 +0000

    Day 1

--- challenge/days/day1.ts
+++ challenge/days/day1.ts
@@ -1,11 +1,46 @@
-import { Day, getInput } from '@cheemcheem/adventofcode-common';
+import { Day, getInputSplitByLine } from '@cheemcheem/adventofcode-common';
 
 const day: Day = {
   part1: async (dayNumber, example) => {
-    return 0;
+    const input = await getInputSplitByLine(dayNumber, example);
+    
+    const {left, right} = input.reduce((prev, curr) => {
+      const [currLeft, currRight] = curr.split('  ');
+      prev.left.push(Number(currLeft));
+      prev.right.push(Number(currRight));
+      return prev;
+    }, {left: [] as number[], right: [] as number[]});
+    
+    left.sort();
+    right.sort();
+
+    const totalDistance = input.reduce((prev, _, index) => {
+      return prev + Math.abs(left[index] - right[index]);;
+    }, 0);
+    
+
+    return totalDistance;
   },
   part2: async (dayNumber, example) => {
-    return 0;
+    const input = await getInputSplitByLine(dayNumber, example);
+    
+    const {left, right} = input.reduce((prev, curr) => {
+      const [currLeft, currRight] = curr.split('  ');
+      prev.left.push(Number(currLeft));
+      prev.right.push(Number(currRight));
+      return prev;
+    }, {left: [] as number[], right: [] as number[]});
+
+    const countInRight = (leftValue: number) => {
+      return right.filter(rightValue => rightValue === leftValue).length
+    }
+
+    const similaritySum = input.reduce((prev, _, index) => {
+      return prev + Math.abs(left[index] * countInRight(left[index]));;
+    }, 0);
+    
+
+    return similaritySum;
   },
 };
 
