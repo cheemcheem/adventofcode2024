1c7479f0f0bb11a9e73f9d7afdd29e921499db5b
Kathan Cheema <19304291+cheemcheem@users.noreply.github.com>
2024-12-04 23:39:47 +0000

    Day 2

--- challenge/days/day2.ts
+++ challenge/days/day2.ts
@@ -1,11 +1,37 @@
-import { Day } from '@cheemcheem/adventofcode-common';
+import { Day, getInputSplitByLine } from '@cheemcheem/adventofcode-common';
+
+const isSafeGap = (level1: number, level2: number) => {
+  const diff = Math.abs(level1 - level2);
+  return diff > 0 && diff < 4;
+}
+
+const isSafe = (report: number[]) => {
+  const isIncreasing = report.every((level, index) => index === 0 || level < report[index - 1]);
+  const isDecreasing = report.every((level, index) => index === 0 || level > report[index - 1]);
+  const hasSafeGaps = report.every((level, index) => index === 0 || isSafeGap(level, report[index - 1]));
+  return (isIncreasing || isDecreasing) && hasSafeGaps;
+}
+
+const isSafeWithDampener = (report: number[]) => {
+  return report.find((_, index) => {
+    const duplicate = [...report];
+    duplicate.splice(index, 1);
+    return isSafe(duplicate);
+  })
+}
 
 const day: Day = {
   part1: async (dayNumber, example) => {
-    return 0;
+    const reportStrings = await getInputSplitByLine(dayNumber, example);
+    const reports = reportStrings.map((report) => report.split(' ').map(Number));
+    const safeReports = reports.filter(isSafe);
+    return safeReports.length;
   },
   part2: async (dayNumber, example) => {
-    return 0;
+    const reportStrings = await getInputSplitByLine(dayNumber, example);
+    const reports = reportStrings.map((report) => report.split(' ').map(Number));
+    const safeReports = reports.filter(isSafeWithDampener);
+    return safeReports.length;
   },
 };
 
