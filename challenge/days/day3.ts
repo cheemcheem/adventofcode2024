a8e0bb001266de6016ddd97be6f2c49a4830324f
Kathan Cheema <19304291+cheemcheem@users.noreply.github.com>
2024-12-05 00:25:54 +0000

    Day 3

--- challenge/days/day3.ts
+++ challenge/days/day3.ts
@@ -1,11 +1,36 @@
-import { Day } from '@cheemcheem/adventofcode-common';
+import { Day, getInput } from '@cheemcheem/adventofcode-common';
 
 const day: Day = {
   part1: async (dayNumber, example) => {
-    return 0;
+    const input = await getInput(dayNumber, example);
+    const instructions = input.matchAll(/mul\(\d\d?\d?,\d\d?\d?\)/g);
+    const result = instructions.reduce((prev, [curr]) => {
+      const commmaSeparated = curr.replaceAll(/(mul)|\(|\)/g, '');
+      const [a, b] = commmaSeparated.split(',').map(Number);
+      return prev + (a * b);
+    }, 0);
+    return result;
   },
   part2: async (dayNumber, example) => {
-    return 0;
+    const input = await getInput(dayNumber, example);
+    const conditionals = input.matchAll(/do(n't)?\(\)/g).toArray().reverse();
+    const instructions = input.matchAll(/mul\(\d\d?\d?,\d\d?\d?\)/g).toArray();
+   
+    const result = instructions.reduce((prev, {"0": curr, index}) => {
+      const [mostRecentConditional] = conditionals.find(({index: conditionalIndex}) => {
+        return conditionalIndex < index;
+      }) ?? []
+
+      if (mostRecentConditional === "don't()") {
+        return prev;
+      }
+
+      const commmaSeparated = curr.replaceAll(/(mul)|\(|\)/g, '');
+      const [a, b] = commmaSeparated.split(',').map(Number);
+      return prev + (a * b);
+    }, 0);
+    
+    return result;
   },
 };
 
