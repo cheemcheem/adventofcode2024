edaec43431d1deec428875acd060c192edd0a672
Kathan Cheema <19304291+cheemcheem@users.noreply.github.com>
2024-12-06 16:34:33 +0000

    Partial Day 6

--- challenge/days/day6.ts
+++ challenge/days/day6.ts
@@ -1,5 +1,28 @@
 import { Day } from '@cheemcheem/adventofcode-common';
 
+type GridValue = '.' | '#' | '^' | 'v' | '<' | '>';
+
+const findGuard = (grid: Readonly<GridValue[][]>) => {
+  return [0, 0];
+};
+
+const traverse = (grid: Readonly<GridValue[][]>) => {
+  const [row, column] = findGuard(grid);
+  const direction = grid[row][column];
+
+  const newGrid = [...grid].map(gridRow => [...gridRow]);
+
+  switch (direction) {
+    case '<': {
+      if (grid[row][column - 1] === '.') {
+        newGrid[row][column - 1] = direction;
+      } else {
+        newGrid[row][column] = '^';
+      }
+    }
+  }
+};
+
 const day: Day = {
   part1: async (dayNumber, example) => {
     return 0;
